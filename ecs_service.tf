39958f2094eba88f036e0fae1fb5f13374a893be
Vivek Satasiya <satasiyavivek@gmail.com>
2023-10-25 16:23:44 +0530

    removed code of task definition

--- ecs_service.tf
+++ ecs_service.tf
@@ -1,24 +1,7 @@
-resource "aws_ecs_task_definition" "ecs_task" {
-  family                   = "${var.task_family}_${terraform.workspace}"
-  network_mode             = "bridge"
-  requires_compatibilities = ["EC2"]
-  cpu                      = var.task_cpu
-  memory                   = var.task_memory
-  container_definitions = jsonencode([{
-    name  = "nginx-container"
-    image = "nginx:latest"
-    portMappings = [{
-      containerPort = 80
-      hostPort      = 0
-    }]
-  }])
-}
-
 resource "aws_ecs_service" "ecs_service" {
-  name            = "ECS_Service_1"
-  cluster         = aws_ecs_cluster.ecs_cluster.id
-  task_definition = aws_ecs_task_definition.ecs_task.arn
-  desired_count   = var.desired_count
+  name          = "ECS_Service_1"
+  cluster       = aws_ecs_cluster.ecs_cluster.id
+  desired_count = var.desired_count
 
   launch_type = "EC2"
 
